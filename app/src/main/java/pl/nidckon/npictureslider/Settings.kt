4c247368cb6941c101d37651ac153b04d63c42a6
Nidckon <nidckon@gmail.com>
2018-09-30 15:48:46 +0200

    zachowywanie nowych ustawień

--- app/src/main/java/pl/nidckon/npictureslider/Settings.kt
+++ app/src/main/java/pl/nidckon/npictureslider/Settings.kt
@@ -1,14 +1,22 @@
 package pl.nidckon.npictureslider
 
 class Settings private constructor(){
-
     var size: Int = 4
     val MIN = 3
     val MAX = 9
 
+    var previewTime = -1
+
     companion object {
         private val settings = Settings()
+        val const = CONST()
 
         fun getSettings() = settings
+
+        class CONST{
+            val PREF_NAME = "pictureslider"
+            val TIME = "time"
+            val SIZE = "size"
+        }
     }
 }
